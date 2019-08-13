e24667362d0fbbe4db72e76506e660b8ae7a28cf
aneme1038 <thriftman25@gmail.com>
2019-08-13 16:34:11 -0400

    Coded the setup code for each function in app.js to handle each function callback in index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,50 @@
 const app = angular.module('guac', []);
 
 app.controller('mainController', ['$http', function($http){
-    
+  const controller = this;
+    this.createGuac = () => {
+      $http({
+        method: 'POST',
+        url: '/guac',
+        data: {
+
+        }
+      }).then(function(response){
+
+      }, function(error){
+        console.log(error);
+      })
+    }
+    this.getGuac = () => {
+      $http({
+        method: 'GET',
+        url: '/guac'
+      }).then(function(response){
+        this.guac = response.data;
+      }, function(error){
+        console.log(error);
+      })
+    }
+    this.deleteGuac = () => {
+      $http({
+        method: "DELETE"
+        url: '/guac/' + guac._id
+      }).then(function(){
+        controller.getGuac();
+      })
+    }
+    this.editGuac = () => {
+      $http({
+        method: 'PUT',
+        url: '/guac/' + guac._id,
+        data: {
+
+        }
+      }).then(function(response){
+        controller.getGuac();
+      }, function(error){
+        console.log(error);
+      })
+    }
+    controller.getGuac();
 }])
