f14df326db2f3c5a5547c0ee8d6dbd943fe6decb
aneme1038 <thriftman25@gmail.com>
2019-08-13 10:38:09 -0400

    serverJS coded

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 3000;
+
+app.use(express(json());
+app.use(express.static('public'));
+
+const guacController = require('./controllers/guac.js');
+
+mongoose.connect('mongodb://localhost:27017/meancrud', {useNewUrlParser:true});
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongoose...');
+}
+app.listen(port, () => {
+  console.log('Listening...');
+})
